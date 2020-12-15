1d597d328a4ea00ddce099587385c61bbef24a43
peter <gudqo1995@naver.com>
2020-12-15 19:44:43 +0900

    test

---
+++ Programmers/src/level2/ex03프린터.java
@@ -0,0 +1,54 @@
+package level2;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ex03프린터 {
+	
+	public static int solution(int[] priorities, int location) {
+		Integer[] p = Arrays.stream(priorities).boxed().toArray(Integer[]::new);
+		Queue<Integer> printer = new LinkedList<Integer>(Arrays.asList(p));
+		Queue<Integer> printerIdx = new LinkedList<Integer>();
+		for(int i=0;i<priorities.length;i++) {
+			printerIdx.offer(i);
+		}
+		
+		System.out.println(printer);
+		System.out.println(printerIdx);
+		int answer = 1;
+		while(!printer.isEmpty()) {
+			boolean flag = true;
+			int tmp = printer.poll();
+			int tmpIdx = printerIdx.poll();
+			for(int i : printer) {
+				if(i>tmp) {
+					printer.offer(tmp);
+					printerIdx.offer(tmpIdx);
+					flag = false;
+					break;
+				}
+			}
+			if(flag) {
+				if(tmpIdx==location)
+					return answer;
+				else
+					answer++;
+			}
+			
+		}
+		System.out.println(printer);
+		System.out.println(printerIdx);
+		
+        return answer;
+    }
+
+	public static void main(String[] args) {
+		int[] priorities = {1, 1, 9, 1, 1, 1};
+		int location = 0;
+		System.out.println(solution(priorities, location));
+		
+
+	}
+
+}
---
+++ Programmers/src/level2/ex03프린터2.java
@@ -0,0 +1,50 @@
+package level2;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class ex03프린터2 {
+	
+	public static int solution(int[] priorities, int location) {
+		Map<Integer, Integer> printer = new HashMap<>();
+		for(int i=0;i<priorities.length;i++) {
+			printer.put(i, priorities[i]);
+		}
+		System.out.println(printer);
+		
+		Set<Entry<Integer, Integer>> set = printer.entrySet();
+		Iterator<Entry<Integer, Integer>> it = set.iterator();
+		while(set.size()>0) {
+			Entry<Integer, Integer> tmp = it.next();
+			int tmpKey = tmp.getKey();
+			int tmpValue = tmp.getValue();
+			
+			
+		}
+		
+//		Iterator<Integer> it = printer.values().iterator();
+//		while(it.hasNext()) {
+//			int tmp = it.next();
+//			for(int i=1;i<printer.size();i++) {
+//				if(printer.get(i)>tmp) {
+//					
+//				}
+//			}
+//			it.remove();
+//		}
+		System.out.println(printer);
+        int answer = 0;
+        return answer;
+    }
+
+	public static void main(String[] args) {
+		int[] priorities = {2, 1, 3, 2};
+		int location = 2;
+		solution(priorities, location);
+
+	}
+
+}
